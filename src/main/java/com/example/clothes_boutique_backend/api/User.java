c9a773c13cee0843f157093b5d4d5de33dd74ea3
Rono Dennis <ronodennis580@gmail.com>
2024-07-10 21:16:32 +0300

    shoppi boutique1

--- src/main/java/com/example/clothes_boutique_backend/api/User.java
+++ src/main/java/com/example/clothes_boutique_backend/api/User.java
@@ -1,9 +1,9 @@
 package com.example.clothes_boutique_backend.api;
 
 
-import jakarta.persistence.Entity;
+/*import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.Id;*/
 import lombok.Data;
 
 @Entity
